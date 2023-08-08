a0523a66fd4d611a63ee0cc05daf93a5426a4c0c
tyral <tyral@zklink.org>
2023-08-08 19:18:48 +0800

    fix: config override

--- src/conf/index.ts
+++ src/conf/index.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 import { ChainId } from '../types'
 
-dotenv.config({ path: `.env.${process.env.APP_ENV}` })
+dotenv.config({ path: `.env.${process.env.APP_ENV}`, override: true })
 dotenv.config({ path: `.env.${process.env.APP_ENV}.local`, override: true })
 
 export const WITNESS_PORT = Number(process.env.WITNESS_PORT)
